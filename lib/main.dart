9591ab353076fd56c418dc9b3588ffbdf2e0018f
kazaliika <mavero811@gmail.com>
2024-06-07 05:07:41 +0700

    membuat assets images

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:ecommerce_final_project/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
