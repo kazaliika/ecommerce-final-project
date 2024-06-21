b3158ca73d49d9be2771c4108046f81f2863d222
kazaliika <mavero811@gmail.com>
2024-06-22 00:22:45 +0700

    fix bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:ecommerce_final_project/models/favorite.dart';
 import 'package:ecommerce_final_project/models/shop.dart';
 import 'package:ecommerce_final_project/models/transaction_services.dart';
 import 'package:ecommerce_final_project/screens/splash_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
